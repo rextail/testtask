95812e35d8ce58fadfd83051cd1fcc82ace3cf8f
rextail <rextail@yandex.ru>
2022-03-15 06:43:40 +0700

    final

--- src/main/java/github/rextail/task/ChangesController.java
+++ src/main/java/github/rextail/task/ChangesController.java
@@ -17,40 +17,22 @@ public class ChangesController {
         getChanges();
     }
 
-
     private void getChanges(){
-
-        Set<String> ykeys = yday.keySet();
-        Set<String> tkeys = tday.keySet();
-
-        HashSet<String> checker = new HashSet<>();
-
-        //not the fastest but good looking and readable method
-        for(String y:ykeys){ //we add all yesterday urls in hashset and check, if it was deleted
-            if(!tday.containsKey(y)){
-                deleted.add(y);
-            }
-            checker.add(y);
-        }
-        for(String t:tkeys){
-            if(checker.add(t)){ //if we add it successfully then it's a new url
-                added.add(t);
-            }
-            else{               //this case means this url isn't new
-                if(!deleted.contains(t) &&tday.get(t)!= yday.get(t)){ //if dict values are different then code was changed
-                    changed.add(t);
-                }
-            }
-        }
-
+       Set<String> allKeys = new HashSet<>(yday.keySet());
+       allKeys.addAll(tday.keySet());
+       for(String key: allKeys){
+           if(!tday.containsKey(key)) deleted.add(key);
+           else if(!yday.containsKey(key)) added.add(key);
+           else if(!Objects.equals(tday.get(key),yday.get(key))) changed.add(key);
+       }
 
     }
     public void printChanges(String fullname){
         System.out.printf("Здравствуйте, дорогая %s%n" +
-                "За последние сутки во вверенных Вам сайтах произошли следующие изменения:",fullname);
-        System.out.printf("Исчезли следующие страницы:%s%n",List.of(deleted).stream().map(String::valueOf).collect(Collectors.joining(",")));
-        System.out.printf("Появились следующие новые страницы:%s%n",List.of(added).stream().map(String::valueOf).collect(Collectors.joining(",")));
-        System.out.printf("Изменились следующие страницы:%s%n",List.of(changed).stream().map(String::valueOf).collect(Collectors.joining(",")));
+                "За последние сутки во вверенных Вам сайтах произошли следующие изменения:\n",fullname);
+        System.out.printf("Исчезли следующие страницы: %s%n",String.join(",", deleted));
+        System.out.printf("Появились следующие новые страницы: %s%n",String.join(",", added));
+        System.out.printf("Изменились следующие страницы: %s%n",String.join(",", changed));
     }
     /*Здравствуйте, дорогая и.о. секретаря
 
